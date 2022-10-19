f15c925a4a04751dbee5de26f83c15736ee315bc
Nitin Nimangare <nitin.nimangare22@gmail.com>
2022-10-19 13:16:57 +0530

    password changed

--- startUp/logging.js
+++ startUp/logging.js
@@ -3,24 +3,12 @@ require("express-async-errors");
 const MongoDB = require("winston-mongodb");
 const config = require("config");
 module.exports = function () {
-  // winston.add(new winston.transports.File({ filename: "logFile.log" }));
-  // const logger = winston.createLogger({
-  //   level: "info",
-  //   format: winston.format.json(),
+  winston.add(new winston.transports.File({ filename: "logFile.log" }));
+  const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
 
-  //   transports: [
-  //     new winston.transports.Console(),
-  //     new winston.transports.MongoDB({
-  //       db: config.get("mongodb"),
-  //       options: { useUnifiedTopology: true },
-  //     }),
-  //   ],
-  // });
-
-  // logger.info("  It's Work!");
-  winston.configure({
     transports: [
-      new winston.transports.File({ filename: "logFile.log" }),
       new winston.transports.Console(),
       new winston.transports.MongoDB({
         db: config.get("mongodb"),
@@ -28,6 +16,18 @@ module.exports = function () {
       }),
     ],
   });
+
+  // logger.info("  It's Work!");
+  // winston.configure({
+  //   transports: [
+  //     new winston.transports.File({ filename: "logFile.log" }),
+  //     new winston.transports.Console(),
+  //     new winston.transports.MongoDB({
+  //       db: config.get("mongodb"),
+  //       options: { useUnifiedTopology: true },
+  //     }),
+  //   ],
+  // });
 
   process.on("uncaughtException", () => {
     winston.error("We have got an uncaught Exception!");
